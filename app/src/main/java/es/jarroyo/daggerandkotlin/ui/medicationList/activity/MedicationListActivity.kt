69f4c19e74b1b11f67bf40e4619794f47e1e3572
Javier Arroyo <javier.arroyo@microhealth.com>
2018-02-27 09:13:00 +0100

    [PUSH NOTIFICATIONS] Config to receive push notifications from Firebase

---
+++ app/src/main/java/es/jarroyo/daggerandkotlin/ui/medicationList/activity/MedicationListActivity.kt
@@ -0,0 +1,12 @@
+package es.jarroyo.daggerandkotlin.ui.medicationList.activity
+
+import android.app.Activity
+import android.os.Bundle
+
+class MedicationListActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_medication_list)
+    }
+}
---
+++ app/src/main/java/es/jarroyo/daggerandkotlin/ui/medicationList/activity/MedicationListView.kt
@@ -0,0 +1,7 @@
+package es.jarroyo.daggerandkotlin.ui.medicationList.activity
+
+/**
+ * Created by javierarroyo on 26/2/18.
+ */
+interface MedicationListView {
+}
